4e79938b488a7a9e45c5745c0bec1c19e0d99c56
Artyom Mankevich <artem.mankevich02@gmail.com>
2022-11-29 20:57:56 +0300

    Feat - Color and Icon init scripts

--- api/README.md
+++ api/README.md
@@ -13,4 +13,24 @@ python manage.py collectstatic
 
 ## API endpoints docs
 
-You can find the API endpoints docs at swagger-ui/
+You can find the API endpoints docs at http://localhost:<your_port>/swagger-ui/
+
+## Default values initialization
+
+### Color
+
+```bash
+docker exec -i trpo_api_1 python manage.py init_colors
+```
+
+### Icon
+
+```bash
+docker exec -i trpo_api_1 python manage.py init_icons
+```
+
+### Currency
+
+```bash
+docker exec -i trpo_api_1 python manage.py init_currencies
+```
---
+++ api/apps/decorations/management/commands/init_colors.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+
+from decorations.models import Color
+
+
+class Command(BaseCommand):
+    help = "Create missing Color objects in the database."
+
+    def handle(self, *args, **options):
+        colors = ["#7A3EF8", "#F6BA1B", "#3E68D1", "#3EB5E8", "#EB4A82", "#555994", ]
+        self.stdout.write(self.style.SUCCESS("START - Creating missing Color objects"))
+
+        counter = 0
+        for color in colors:
+            obj, created = Color.objects.get_or_create(hex_code=color)
+            if created:
+                counter += 1
+                self.stdout.write(self.style.SUCCESS(f"Created Color {color}"))
+            else:
+                self.stdout.write(self.style.WARNING(f"Color {color} already exists"))
+
+        self.stdout.write(self.style.SUCCESS(f"END - Created {counter} Color objects"))
