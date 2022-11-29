16a424eab55eebba83e2166c1b7b9fef914471b3
Artyom Mankevich <artem.mankevich02@gmail.com>
2022-11-29 22:41:05 +0300

    Fix - removed retrieve endpoints for Color & Icon

--- api/apps/decorations/views.py
+++ api/apps/decorations/views.py
@@ -1,14 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from decorations.models import Color, Icon
 from decorations.serializers import ColorSerializer, IconSerializer
 
 
-class ColorViewSet(viewsets.ReadOnlyModelViewSet):
+class ColorViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = Color.objects.all()
     serializer_class = ColorSerializer
 
 
-class IconViewSet(viewsets.ReadOnlyModelViewSet):
+class IconViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = Icon.objects.all()
     serializer_class = IconSerializer
