31461a64099f98bd308a34e62a951b4885afbce8
Artyom Mankevich <artem.mankevich02@gmail.com>
2022-11-30 23:30:29 +0300

    Feat - camelCase renderer

--- api/project/settings.py
+++ api/project/settings.py
@@ -186,6 +186,15 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ),
+    "DEFAULT_RENDERER_CLASSES": (
+        "djangorestframework_camel_case.render.CamelCaseJSONRenderer",
+        "djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer",
+    ),
+    "DEFAULT_PARSER_CLASSES": (
+        "djangorestframework_camel_case.parser.CamelCaseFormParser",
+        "djangorestframework_camel_case.parser.CamelCaseMultiPartParser",
+        "djangorestframework_camel_case.parser.CamelCaseJSONParser",
+    ),
 }
 
 SWAGGER_SETTINGS = {
