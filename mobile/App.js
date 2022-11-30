64fd16b40d134d4e1c30964701f4a114ff8d14f7
PashaEfimchik <pefimchshiks@gmail.com>
2022-11-30 23:14:26 +0300

    Feat - added Auth0Provider

--- mobile/App.js
+++ mobile/App.js
@@ -2,13 +2,17 @@ import { createDrawerNavigator} from "@react-navigation/drawer";
 import SidebarMenu from "./components/SidebarMenu";
 import * as React from "react";
 import {NavigationContainer} from "@react-navigation/native";
+import {Auth0Provider} from "react-native-auth0";
+import config from "./auth0.config";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <SidebarMenu {...Drawer}/>
-      </NavigationContainer>
+        <Auth0Provider clientId={config.clientId} domain={config.domain}>
+            <NavigationContainer>
+                <SidebarMenu {...Drawer}/>
+            </NavigationContainer>
+        </Auth0Provider>
   );
 }
