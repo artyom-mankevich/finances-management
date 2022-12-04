56875d79bf8c16042cc9ae8b9aa51aa8a910d63f
Artyom Mankevich <artem.mankevich02@gmail.com>
2022-12-04 18:31:13 +0300

    NewsFilter update on Stock creation

--- api/apps/investments/models.py
+++ api/apps/investments/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 
 from decorations.models import Color
+from news.models import NewsFilter
 from wallets.models import Wallet, Currency
 
 
@@ -14,3 +15,13 @@ class Stock(models.Model):
     color = models.ForeignKey(Color, on_delete=models.SET_DEFAULT, default="000000")
     description = models.CharField(max_length=256, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        update = Stock.objects.filter(id=self.id).exists()
+
+        if not update:
+            news_filter, created = NewsFilter.objects.get_or_create(user_id=self.user_id)
+            news_filter.tickers.append(self.ticker)
+            news_filter.save()
+
+        super().save(*args, **kwargs)
