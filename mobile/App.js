56b0f60135dd02959ad798e69487dc6b65e4298d
PashaEfimchik <pefimchshiks@gmail.com>
2022-12-04 15:20:13 +0300

    Fix - app config

--- mobile/App.js
+++ mobile/App.js
@@ -2,17 +2,13 @@ import { createDrawerNavigator} from "@react-navigation/drawer";
 import SidebarMenu from "./components/SidebarMenu";
 import * as React from "react";
 import {NavigationContainer} from "@react-navigation/native";
-import {Auth0Provider} from "react-native-auth0";
-import config from "./auth0.config";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-        <Auth0Provider clientId={config.clientId} domain={config.domain}>
-            <NavigationContainer>
-                <SidebarMenu {...Drawer}/>
-            </NavigationContainer>
-        </Auth0Provider>
-  );
+    return (
+        <NavigationContainer>
+            <SidebarMenu {...Drawer}/>
+        </NavigationContainer>
+    );
 }
--- mobile/auth0.config.js
+++ mobile/auth0.config.js
@@ -1,6 +1,7 @@
 const config = {
     clientId: "rCJhRDfeojb1tvuRiZHmSgXuJkvWmqU8",
     domain: "https://dev-16fr5mnt2b0eess4.us.auth0.com",
+    audience: "https://finances-be.com/",
 };
 
 export default config;
