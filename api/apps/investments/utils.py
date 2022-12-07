d4be74c06da69f24dae7b36caca8eb76264c3d29
Artyom Mankevich <artem.mankevich02@gmail.com>
2022-12-07 13:20:57 +0300

    Stock prices retrieve refactoring, moved to yahooquery

--- api/apps/investments/utils.py
+++ api/apps/investments/utils.py
@@ -1,35 +1,62 @@
 from decimal import Decimal
 
 import requests
-import yfinance as yf
+import yahooquery as yq
+
 from django.core.cache import cache
 
 from project import settings
 
 
 def set_stock_prices(response):
+    tickers = [stock["ticker"] for stock in response]
+    stock_prices = get_stock_prices(tickers)
     for stock in response:
-        stock["price"] = get_stock_price(stock["ticker"]) * stock["amount"]
+        stock["price"] = stock_prices[stock["ticker"]]
 
 
-def get_stock_price(ticker):
-    cache_key = f"stock_price:{ticker}"
-    cached_info = cache.get(cache_key)
+def get_stock_prices(tickers: list) -> dict:
+    data = {ticker: {"currency": None, "price": None} for ticker in tickers}
 
-    if cached_info:
-        price = cached_info["price"]
-        currency = cached_info["currency"]
-    else:
-        price = None
-        currency = None
+    set_tickers_data_from_cache(data, tickers)
+    tickers_to_fetch = [ticker for ticker in tickers if data[ticker]["price"] is None]
 
-        ticker = yf.Ticker(ticker)
-        if ticker.info.get("currentPrice"):
-            price = ticker.info["currentPrice"]
-            currency = ticker.info["currency"]
-            cache.set(cache_key, {"price": price, "currency": currency}, 60 * 10)
+    if tickers_to_fetch:
+        set_tickers_data_from_api(data, tickers_to_fetch)
 
-    return convert_currency(price, currency)
+    return get_converted_ticker_prices(data, tickers)
+
+
+def set_tickers_data_from_api(data: dict, tickers_to_fetch: list[str]):
+    yq_tickers = yq.Ticker(" ".join(tickers_to_fetch)).financial_data
+    for ticker in tickers_to_fetch:
+        price = yq_tickers[ticker].get("currentPrice")
+        currency = yq_tickers[ticker].get("financialCurrency")
+
+        data[ticker]["price"] = price
+        data[ticker]["currency"] = currency
+
+        cache.set(f"stock_price_{ticker}", price, 60 * 5)
+        cache.set(f"stock_currency_{ticker}", currency, 60 * 5)
+
+
+def get_converted_ticker_prices(data: dict, tickers: list[str]) -> dict:
+    result = {}
+    for ticker in tickers:
+        result[ticker] = convert_currency(
+            data[ticker]["price"], data[ticker]["currency"]
+        )
+    return result
+
+
+def set_tickers_data_from_cache(data, tickers):
+    for ticker in tickers:
+        price = cache.get(f"stock_price_{ticker}")
+        currency = cache.get(f"stock_currency_{ticker}")
+        if price is not None:
+            data[ticker]["price"] = price
+        if currency is not None:
+            data[ticker]["currency"] = currency
 
 
 def convert_currency(amount, currency):
