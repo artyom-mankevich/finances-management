25f270adbfe5e9d53ae803120264283788b4c648
Artyom Mankevich <artem.mankevich02@gmail.com>
2022-12-07 18:27:21 +0300

    Feat - stocks chart data

--- api/apps/investments/utils.py
+++ api/apps/investments/utils.py
@@ -1,10 +1,13 @@
+import datetime
 from decimal import Decimal
 
+import numpy as np
 import requests
 import yahooquery as yq
 
 from django.core.cache import cache
 
+from investments.models import Stock
 from project import settings
 
 
@@ -12,7 +15,7 @@ def set_stock_prices(response):
     tickers = [stock["ticker"] for stock in response]
     stock_prices = get_stock_prices(tickers)
     for stock in response:
-        stock["price"] = stock_prices[stock["ticker"]]
+        stock["price"] = stock_prices[stock["ticker"]] * stock["amount"]
 
 
 def get_stock_prices(tickers: list) -> dict:
@@ -28,7 +31,7 @@ def get_stock_prices(tickers: list) -> dict:
 
 
 def set_tickers_data_from_api(data: dict, tickers_to_fetch: list[str]):
-    yq_tickers = yq.Ticker(" ".join(tickers_to_fetch)).financial_data
+    yq_tickers = yq.Ticker(" ".join(tickers_to_fetch), asynchronous=True).financial_data
     for ticker in tickers_to_fetch:
         price = yq_tickers[ticker].get("currentPrice")
         currency = yq_tickers[ticker].get("financialCurrency")
@@ -80,3 +83,92 @@ def convert_currency(amount, currency):
         return result
 
     return None
+
+
+def get_period_dates(
+    period: str, today: datetime.date
+) -> tuple[str, str] | tuple[None, None]:
+    if period == Stock.CHART_PERIOD_7_DAYS:
+        start_date = (
+                today - datetime.timedelta(days=today.weekday())
+        ).strftime("%Y-%m-%d")
+        end_date = today.strftime("%Y-%m-%d")
+
+        return start_date, end_date
+
+    return None, None
+
+
+def get_period_interval(period: str) -> str:
+    if period in [Stock.CHART_PERIOD_7_DAYS, Stock.CHART_PERIOD_1_MONTH]:
+        return "1d"
+    elif period == Stock.CHART_PERIOD_3_MONTHS:
+        return "1wk"
+    elif period == Stock.CHART_PERIOD_1_YEAR:
+        return "1mo"
+
+
+def get_stocks_chart_data(tickers: list[str], period: str, user_id: str) -> dict:
+    symbols = " ".join(tickers)
+
+    today = datetime.date.today()
+    start_date, end_date = get_period_dates(period, today)
+    interval = get_period_interval(period)
+
+    tickers_key = "_".join(tickers)
+    cache_key = f"stocks_chart_data_{tickers_key}_{period}_{start_date}_{end_date}_{interval}_{user_id}"
+    cached_value = cache.get(cache_key)
+    if cached_value:
+        return cached_value
+
+    cache_lifetime = 60 * 60 * 24
+
+    historical_prices = get_historical_stocks_data(
+        symbols, period, start_date, end_date, interval
+    )
+    result = {"data": historical_prices}
+
+    if period in [Stock.CHART_PERIOD_7_DAYS, Stock.CHART_PERIOD_1_MONTH]:
+        today_prices = {
+            today.strftime("%d-%m-%Y"): sum(get_stock_prices(tickers).values())
+        }
+        result["data"] = {**historical_prices, **today_prices}
+        cache_lifetime = 60 * 10
+
+    average_price = sum(result["data"].values()) / len(result["data"])
+    result["average_price"] = average_price
+
+    cache.set(cache_key, result, cache_lifetime)
+
+    return result
+
+
+def get_historical_stocks_data(
+    symbols: str,
+    period: str,
+    start_date: str = None,
+    end_date: str = None,
+    interval: str = "1d"
+) -> dict[str, Decimal]:
+    series = yq.Ticker(symbols=symbols, asynchronous=True, validate=True).history(
+        period=period, start=start_date, end=end_date, interval=interval
+    )["close"]
+
+    result = {}
+    for i in range(len(series)):
+        if np.isnan(series[i]):
+            continue
+
+        date = series.index[i][1].strftime("%d-%m-%Y")
+        price = Decimal(str(series[i]))
+
+        if date not in result:
+            result[date] = price
+        else:
+            result[date] += price
+
+    if period not in [Stock.CHART_PERIOD_7_DAYS, Stock.CHART_PERIOD_1_MONTH]:
+        # remove today's data
+        result.popitem()
+
+    return result
--- api/apps/investments/views.py
+++ api/apps/investments/views.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from accounts.views import SetUserIdFromTokenOnCreateMixin
 from investments.models import Stock
 from investments.serializers import StockSerializer
-from investments.utils import set_stock_prices
+from investments.utils import set_stock_prices, get_stocks_chart_data
 
 
 class StockViewSet(viewsets.ModelViewSet, SetUserIdFromTokenOnCreateMixin):
@@ -26,3 +28,15 @@ class StockViewSet(viewsets.ModelViewSet, SetUserIdFromTokenOnCreateMixin):
         set_stock_prices(response.data["results"])
 
         return response
+
+    @action(detail=False, methods=["get"], url_path="chart-data", url_name="chart-data")
+    def chart_data(self, request, *args, **kwargs):
+        period = request.query_params.get("period", Stock.CHART_PERIOD_7_DAYS)
+        if period not in Stock.CHART_PERIODS:
+            return Response(
+                {"error": f"Period {period} is not allowed"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        tickers = list(self.get_queryset().values_list("ticker", flat=True))
+        return Response(data=get_stocks_chart_data(tickers, period, str(request.user)))
