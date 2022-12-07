735d7092e25347b5a02644a41fe22e402bcbd388
PashaEfimchik <pefimchshiks@gmail.com>
2022-12-07 12:34:28 +0300

    Fix - added list of wallets

--- mobile/screens/Wallets.jsx
+++ mobile/screens/Wallets.jsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
-import { View, Text } from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
-
+import {View, StyleSheet, ScrollView} from "react-native";
+import CreateWallet from "../components/wallets/CreateWallet";
+import WalletsList from "../components/wallets/WalletsList";
 export default function WalletPage() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Ionicons name="wallet" size={30} color='#3e68d1' />
-            <Text style={{fontSize:16,fontWeight:'700'}}>Wallet</Text>
-        </View>
+            <ScrollView>
+                <View style={styles.container }>
+                    <WalletsList />
+                    <CreateWallet/>
+                </View>
+            </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 20,
+    },
+});
