be4b83a71dad4d0ba0f3d18c8331cdb184a5f772
Artyom Mankevich <61446379+artyom-mankevich@users.noreply.github.com>
2022-12-09 22:50:10 +0300

    Fix - top categories filtering (#95)
    * Fix - top categories filtering
    
    * minor refactoring

--- api/apps/wallets/utils.py
+++ api/apps/wallets/utils.py
@@ -25,22 +25,26 @@ def get_top_categories_queryset(
     initial_qs: TransactionCategory.objects,
     wallet_type: str,
     amount_type: str
-) -> TransactionCategory.objects:
+) -> list:
     wallet_lookup = {f"transaction__{wallet_type}_wallet__isnull": True}
+    transaction_lookup = {f"{amount_type}_wallet__isnull": False}
 
-    categories = initial_qs.filter(**wallet_lookup)
+    categories = initial_qs.filter(**wallet_lookup).distinct()
+    list_of_categories = []
     for category in categories:
         total = get_transactions_total_amount(
-            category.transaction_set.all().prefetch_related(
+            category.transaction_set.filter(**transaction_lookup).prefetch_related(
                 f"{wallet_type}_wallet__currency"
             ),
             amount_type
         )
-        category.total = total
+        if total != 0:
+            category.total = total
+            list_of_categories.append(category)
 
-    categories = sorted(categories, key=lambda x: x.total, reverse=True)
+    list_of_categories = sorted(list_of_categories, key=lambda x: x.total, reverse=True)
 
-    return categories[:3]
+    return list_of_categories[:3]
 
 
 def get_transactions_total_amount(
