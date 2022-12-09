93ad873d531d5bcaee9796f8a38cdf6a4865dbb3
PashaEfimchik <pefimchshiks@gmail.com>
2022-12-09 13:43:18 +0300

    Fix - added swipe to refresh

--- mobile/screens/Wallets.jsx
+++ mobile/screens/Wallets.jsx
@@ -1,12 +1,26 @@
 import * as React from 'react';
-import {View, StyleSheet, ScrollView} from "react-native";
+import {View, StyleSheet, ScrollView, RefreshControl} from "react-native";
 import WalletsList from "../components/wallets/WalletsList";
+import {useCallback, useState} from "react";
+
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 
 export default function WalletPage() {
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        wait(100).then(() => setRefreshing(false));
+    }, []);
+
     return (
             <ScrollView>
                 <View style={styles.container }>
-                    <WalletsList />
+                    <RefreshControl refreshing={refreshing} onRefresh={onRefresh}>
+                        <WalletsList refreshing={refreshing}/>
+                    </RefreshControl>
                 </View>
             </ScrollView>
     );
