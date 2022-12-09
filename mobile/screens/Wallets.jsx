e80d48ec3fa07de82730a8c469325a25ff8f9eb1
PashaEfimchik <pefimchshiks@gmail.com>
2022-12-09 05:41:56 +0300

    Fix - changed button position

--- mobile/screens/Wallets.jsx
+++ mobile/screens/Wallets.jsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import {View, StyleSheet, ScrollView} from "react-native";
-import CreateWallet from "../components/wallets/CreateWallet";
 import WalletsList from "../components/wallets/WalletsList";
-export default function WalletPage() {
 
+export default function WalletPage() {
     return (
             <ScrollView>
                 <View style={styles.container }>
                     <WalletsList />
-                    <CreateWallet/>
                 </View>
             </ScrollView>
     );
