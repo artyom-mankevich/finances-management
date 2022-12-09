3eed4feed0c3665fac1040de0fede5898d18ec51
Artyom Mankevich <artem.mankevich02@gmail.com>
2022-12-09 18:46:21 +0300

    Fix - top categories currency

--- api/apps/wallets/utils.py
+++ api/apps/wallets/utils.py
@@ -1,6 +1,5 @@
-from django.db.models import Sum
-
-from wallets.models import TransactionCategory
+from base.utils import convert_currency
+from wallets.models import TransactionCategory, Transaction
 from wallets.serializers import ExtendedTransactionCategorySerializer
 
 
@@ -11,11 +10,11 @@ def get_top_categories(queryset: TransactionCategory.objects) -> dict:
     response = {
         "incomes": {
             "data": ExtendedTransactionCategorySerializer(incomes, many=True).data,
-            "total": incomes.aggregate(total_sum=Sum("total"))["total_sum"],
+            "total": get_transaction_type_total_amount(incomes),
         },
         "expenses": {
             "data": ExtendedTransactionCategorySerializer(expenses, many=True).data,
-            "total": expenses.aggregate(total_sum=Sum("total"))["total_sum"],
+            "total": get_transaction_type_total_amount(expenses),
         },
     }
 
@@ -28,10 +27,38 @@ def get_top_categories_queryset(
     amount_type: str
 ) -> TransactionCategory.objects:
     wallet_lookup = {f"transaction__{wallet_type}_wallet__isnull": True}
-    sum_lookup = f"transaction__{amount_type}_amount"
 
-    result = initial_qs.filter(**wallet_lookup).annotate(total=Sum(sum_lookup)).order_by(
-        "-total"
-    ).exclude(total__isnull=True)[:3]
+    categories = initial_qs.filter(**wallet_lookup)
+    for category in categories:
+        total = get_transactions_total_amount(
+            category.transaction_set.all().prefetch_related(
+                f"{wallet_type}_wallet__currency"
+            ),
+            amount_type
+        )
+        category.total = total
 
-    return result
+    categories = sorted(categories, key=lambda x: x.total, reverse=True)
+
+    return categories[:3]
+
+
+def get_transactions_total_amount(
+    transactions: Transaction.objects, amount_type: str
+) -> float:
+    total = sum(
+        convert_currency(
+            getattr(transaction, f"{amount_type}_amount"),
+            getattr(transaction, f"{amount_type}_wallet").currency.code
+        ) for transaction in transactions
+    )
+
+    return total
+
+
+def get_transaction_type_total_amount(categories: list) -> float | None:
+    _sum = sum(category.total for category in categories)
+    if _sum == 0:
+        return None
+
+    return _sum
