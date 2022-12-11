02667c818809f5f2dedd89809c848fdbad6ede32
PashaEfimchik <pefimchshiks@gmail.com>
2022-12-11 13:42:37 +0300

    Fix - updated refresh

--- mobile/screens/Transactions.jsx
+++ mobile/screens/Transactions.jsx
@@ -1,12 +1,34 @@
 import * as React from 'react';
-import { View, Text } from "react-native";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
+import {View, StyleSheet, ScrollView, RefreshControl} from "react-native";
+import CategoryList from "../components/transactions/categories/CategoryList";
+import {useCallback, useState} from "react";
+
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 
 export default function TransactionsPage() {
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        wait(100).then(() => setRefreshing(false));
+    }, []);
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <FontAwesome name="dollar" size={30} color='#3e68d1' />
-            <Text style={{fontSize:16,fontWeight:'700'}}>Transactions</Text>
-        </View>
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh}>
+            <ScrollView>
+                <View style={styles.container }>
+                    <CategoryList refreshing={refreshing}/>
+                </View>
+            </ScrollView>
+        </RefreshControl>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+});
