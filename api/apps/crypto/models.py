8a4a5ab102434886e4aa9523e5020c9badc24a75
Artyom Mankevich <61446379+artyom-mankevich@users.noreply.github.com>
2022-12-14 16:27:02 +0300

    Feat - eth transfering (#116)
    * Feat - eth transfering
    
    * fix - typo targeet -> target
    
    Co-authored-by: AdamKorzun <a.korzun21@gmail.com>

--- api/apps/crypto/models.py
+++ api/apps/crypto/models.py
@@ -6,5 +6,5 @@ from django.db import models
 class EthKeys(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     user_id = models.CharField(max_length=64, db_index=True)
-    private_key = models.CharField(max_length=128)
-    address = models.CharField(max_length=128)
+    private_key = models.CharField(max_length=256)
+    address = models.CharField(max_length=128, unique=True)
--- api/apps/crypto/serializers.py
+++ api/apps/crypto/serializers.py
@@ -1,5 +1,7 @@
+from django.db import transaction
 from rest_framework import serializers
 
+from base.encryption import Encryption
 from crypto.models import EthKeys
 
 
@@ -10,3 +12,22 @@ class EthKeysSerializer(serializers.ModelSerializer):
         fields = read_only_fields + ("address", "private_key")
 
     private_key = serializers.CharField(write_only=True)
+
+    @transaction.atomic
+    def create(self, validated_data):
+        password = self._validate_password(self.initial_data.get("password"))
+        private_key = validated_data.pop("private_key")
+
+        validated_data["private_key"] = Encryption.aes_encrypt(private_key, password)
+
+        return super().create(validated_data)
+
+    def _validate_password(self, password):
+        if not password:
+            raise serializers.ValidationError({"password": "This field is required."})
+        if type(password) != str:
+            raise serializers.ValidationError(
+                {"password": "This field must be a string."}
+            )
+
+        return password
