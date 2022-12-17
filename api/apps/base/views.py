621219f48a199cc9d15553f3eb01d8d342e4f274
Adam Korzun <45329904+AdamKorzun@users.noreply.github.com>
2022-12-18 00:08:26 +0300

    fix - codestyle (#141)

--- api/apps/base/views.py
+++ api/apps/base/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 
+
 class RawListModelMixin:
     def list(self, request, *args, **kwargs):
-        object = [obj for obj in self.get_rawqueryset()]
-        serializer = self.get_serializer(object, many=True)
+        objects = [obj for obj in self.get_rawqueryset()]
+        serializer = self.get_serializer(objects, many=True)
         return Response(serializer.data)
