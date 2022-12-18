37fe2bcf0d4cf0d2ffb57f1ad5c02cb3b7fdb7fa
AdamKorzun <a.korzun21@gmail.com>
2022-12-18 15:17:16 +0300

    feat - accounts raw sql

--- api/apps/base/backends.py
+++ api/apps/base/backends.py
@@ -1,11 +1,19 @@
 from accounts.models import AccountSettings
 from django.contrib.auth.backends import RemoteUserBackend
+from django.db import connection
 
 
 class CustomRemoteUserBackend(RemoteUserBackend):
     def configure_user(self, request, user, created=True):
         user = super().configure_user(request, user, created)
         if created:
-            settings_exist = AccountSettings.objects.filter(user_id=user).exists()
-            if not settings_exist:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    f"SELECT EXISTS("
+                    f"SELECT 1 FROM {AccountSettings._meta.db_table} "
+                    f"WHERE user_id=%s);",
+                    [str(user)]
+                )
+                exists = cursor.fetchone()[0]
+            if not exists:
                 AccountSettings.objects.create(user_id=user)
